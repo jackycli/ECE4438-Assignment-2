730dfa9a0bef31911e4642c3c8d988c73b2472ae
Jacky Li <jackychengjieli@gmail.com>
2024-02-27 19:52:58 -0500

    Working Main 1, Main 2 with maxpooling

--- main2.m
+++ main2.m
@@ -24,6 +24,10 @@ layers = [
     batchNormalizationLayer
     reluLayer 
     maxPooling2dLayer(2,'Stride',2)
+    convolution2dLayer(3,16, 'Padding','same') 
+    batchNormalizationLayer
+    reluLayer 
+    maxPooling2dLayer(2,'Stride',2)
     convolution2dLayer(3,32, 'Padding','same') 
     batchNormalizationLayer
     reluLayer 
@@ -35,11 +39,6 @@ layers = [
     convolution2dLayer(3,128, 'Padding','same') 
     batchNormalizationLayer
     reluLayer 
-    maxPooling2dLayer(2,'Stride',2)
-    convolution2dLayer(3,256, 'Padding','same') 
-    batchNormalizationLayer
-    reluLayer 
-    
     fullyConnectedLayer(10)
     softmaxLayer
     classificationLayer];
